0312ebbb69027ebd8428a146f0087cddc74cdbd5
Andrew <rew1998and@gmail.com>
2023-12-30 17:17:32 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  const currentLeft = parseInt(dodger.style.left);
+  const newLeft = currentLeft - 10; // Adjust the amount to move
+
+  dodger.style.left = `${newLeft}px`;
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const currentLeft = parseInt(dodger.style.left);
+  const newLeft = currentLeft + 10; // Adjust the amount to move
+
+  dodger.style.left = `${newLeft}px`;
+}
